27bad28fdae69bbc2cc50770522c73fe0c4e615b
vishalbirajdarconsociaet <vishal.birajdar@consociatesolutions.com>
2023-02-02 11:15:44 +0530

    a

--- LMS_api/settings.py
+++ LMS_api/settings.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -137,4 +137,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-7d8d.up.railway.app/']
+# CSRF_TRUSTED_ORIGINS = ['https://web-production-7d8d.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['*']
